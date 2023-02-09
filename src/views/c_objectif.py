6ad7a95a44ebef019b301798dce97fb519863cde
Mohamed I <m.idttalbe@outlook.com>
2023-02-09 17:48:12 +0100

    traduction section Objectif>Présentation des colonnes;

--- src/views/c_objectif.py
+++ src/views/c_objectif.py
@@ -47,112 +47,112 @@ def load_colonnes():
     with st.container():
         st.markdown("""
         > 01. **'hotel' (String)** : \n
-                type d'hôtel {'City Hotel','Resort Hotel'}
+                type d'hôtel {'City Hotel','Resort Hotel'}.
         """)
         st.markdown("""
         > 02. **'is_canceled' (Bool)** : \n
-                Value indicating if the booking was canceled (1) or not (0)
+                Valeur indiquant si la réservation a été annulée (1) ou non (0).
                 """)
         st.markdown("""
         > 03. **'lead_time' (Integer)** : \n
-                Number of days that elapsed between the entering date of the booking into the PMS and the arrival date.
+                Nombre de jours qui se sont écoulés entre la date de saisie de la réservation dans le PMS et la date d'arrivée.
                 """)
         st.markdown("""
         > 04. **'arrival_date_year' (Integer)** : \n
-                Year of arrival date
+                Année d'arrivée.
         """)
         st.markdown("""
         > 05. **'arrival_date_month' (String)** : \n
-                Month of arrival date, exemple : July, August
+                Mois d’arrivée, exemple : Juillet, Août, ... .
                 """)
         st.markdown("""        
         > 06. **'arrival_date_week_number' (Integer)** : \n
-                Week number of year for arrival date.
+                Numéro de semaine d'arrivée.
                 """)
         st.markdown(""" 
         > 07. **'arrival_date_day_of_month' (Integer)** : \n
-                Day of arrival date
+                Jour d'arrivée.
                 """)
         st.markdown("""
         > 08. **'stays_in_weekend_nights' (Integer)** : \n
-                Number of weekend nights (Saturday or Sunday) the guest stayed or booked to stay at the hotel
+                Nombre de nuités en week-end (samedi ou dimanche) où le client a séjourné ou réservé pour séjourner à l'hôtel.
                 """)
         st.markdown("""
         > 09. **'stays_in_week_nights' (Integer)** : \n
-                Number of week nights (Monday to Friday) the guest stayed or booked to stay at the hotel
+                Nombre de nuités en semaine (du lundi au vendredi) où le client a séjourné ou réservé pour séjourner à l'hôtel.
                 """)
         st.markdown("""        
         > 10. **'adults' (Integer)** :  \n
-            Number of adults
+            Nombre d'adultes.
                 """)
         st.markdown("""
         > 11. **'children' (Integer)** : \n
-            Number of children
+            Nombre d'enfants.
             """)
 
         st.markdown("""
         > 12. **'babies' (Integer)** : \n
-            Number of babies
+            Nombre de bébés.
             """)
 
         st.markdown("""
         > 13. **'meal' (String)** : \n
-            Type of meal booked. Categories are presented in standard hospitality meal packages: 
+            Type de repas réservé. Les catégories sont présentées dans des forfaits repas d'accueil standard. 
             """)
 
         st.markdown("""
         > 14. **'country' (String)** : \n
-            Country of origin. Categories are represented in the ISO 3155–3:2013 format, example: PRT, GBR
+            Pays d'origine. Les catégories sont représentées au format ISO 3155-3:2013, exemple : PRT, GBR, ... .
             """)
 
         st.markdown("""
         > 15. **'market_segment' (String)** : \n 
-            Market segment designation.
+            Désignation du segment de marché.
             """) 
 
         st.markdown("""
         > 16. **'distribution_channel' (String)** : \n
-            Booking distribution channel.
+            Canal de distribution de réservation.
             """)
 
         st.markdown("""
         > 17. **'is_repeated_guest' (Bool)** : \n
-            Value indicating if the booking name was from a repeated guest (1) or not (0)
+            Valeur indiquant si le nom de la réservation provient d'un client répété (1) ou non (0).
             """)
 
         st.markdown("""
         > 18. **'previous_cancellations' (Integer)** : \n
-            Number of previous bookings that were cancelled by the customer prior to the current booking
+            Nombre de réservations précédentes qui ont été annulées par le client avant la réservation en cours.
             """)
 
         st.markdown("""
         > 19. **'previous_bookings_not_canceled' (Integer)** : \n
-            Number of previous bookings not cancelled by the customer prior to the current booking
+            Nombre de réservations précédentes non annulées par le client avant la réservation en cours.
             """)
 
         st.markdown("""
         > 20. **'reserved_room_type' (String)** : \n
-            Code of room type reserved. Code is presented instead of designation for anonymity reasons.
+            Code du type de chambre réservé. Le code est présenté au lieu d’une désignation pour des raisons d’anonymat.
             """)
 
         st.markdown("""
         > 21. **'assigned_room_type' (String)** : 
-            Code for the type of room assigned to the booking.
-            Sometimes the assigned room type differs from the reserved room type due to hotel operation reasons (e.g. overbooking) or by customer request.
-            Code is presented instead of designation for anonymity reasons.""")
+            Code pour le type de chambre attribué à la réservation.
+            Parfois le type de chambre assigné différe de celui reservé à cause d'opérations diverses effectuées par l'hotel (ex: overbooking) ou bien par le client.
+            Le code est affiché uniquement pour des raisons d'anonynimisation.""")
 
         st.markdown("""        
         > 22. **'booking_changes' (Integer)** : \n
-            Number of changes/amendments made to the booking from the moment the booking was entered on the PMS until the moment of check-in or cancellation
+            Nombre de changements/modifications apportés à la réservation depuis le moment où la réservation a été saisie dans le PMS jusqu'au moment de l'enregistrement ou de l'annulation.
                 """)
         
         st.markdown("""
         > 23. **'deposit_type' (String)** : \n
-            Indication on if the customer made a deposit to guarantee the booking. 
-            This variable can assume three categories: 
-                - No Deposit - no deposit was made; 
-                - Non Refund - a deposit was made in the value of the total stay cost; 
-                - Refundable - a deposit was made with a value under the total cost of stay.
+            Indique si le client a effectué un dépôt pour garantir la réservation. 
+            Cette variable peut prendre trois catégories : 
+                - No Deposit - aucun dépôt n'a été effectué ; 
+                - Non Refund - un dépôt a été effectué avec une valeur égale au coût total du séjour ; 
+                - Refundable - un dépôt a été effectué avec une valeur inférieure au coût total du séjour.
                     """)
         
         st.markdown("""
@@ -167,49 +167,49 @@ def load_colonnes():
 
         st.markdown("""
         > 26. **'days_in_waiting_list'** :\n 
-            Number of days the booking was in the waiting list before it was confirmed to the customer
+            Nombre de jours où la réservation est restée sur la liste d'attente avant d'être confirmée au client.
             """)
 
         st.markdown("""
         > 27. **'customer_type'** : \n
-            Type of booking, assuming one of four categories:
-                - Contract - when the booking has an allotment or other type of contract associated to it 
-                - Group – when the booking is associated to a group 
-                - Transient – when the booking is not part of a group or contract, and is not associated to other transient booking
-                - Transient-party – when the booking is transient, but is associated to at least other transient booking
+            Type de réservation, en assumant l'une des quatre catégories suivantes :
+                - Contract - lorsque la réservation est associée à un allotissement ou à un autre type de contrat. 
+                - Group - lorsque la réservation est associée à un groupe. 
+                - Transient - lorsque la réservation ne fait pas partie d'un groupe ou d'un contrat, et n'est pas associée à une autre réservation transitoire.
+                - Transient-party - lorsque la réservation est transitoire, mais qu'elle est associée à au moins une autre réservation transitoire.
         """)
 
         st.markdown("""
         > 28. **'adr'** :
-            Average Daily Rate as defined by dividing the sum of all lodging transactions by the total number of staying nights. 
-            Explications : inclus le prix total de toutes les préstations incluant le prix de la chambre sur le nombre de nuits pour avoir un prix moyen par nuité.
+            Le tarif journalier moyen est défini en divisant la somme de toutes les transactions d'hébergement par le nombre total de nuits d'hébergement. 
+            Explication : Inclure le prix total de toutes les transactions d'hébergement, y compris le prix de la chambre, sur le nombre de nuits pour obtenir un prix moyen par nuit.
         """)
 
         st.markdown("""
         > 29. **'required_car_parking_spaces'** :
-            Number of car parking spaces required by the customer
+            Nombre d'emplacements de parking requis par le client.
             """)
 
         st.markdown("""
         > 30. **'total_of_special_requests'** : 
-            Number of special requests made by the customer (e.g. twin bed or high floor)
+            Nombre de demandes spéciales faites par le client (par exemple, lit double ou étage élevé).
             """)
         
         st.markdown("""
         > 31. **'reservation_status'** : 
-            Reservation last status, assuming one of three categories:
-                - Canceled - booking was canceled by the customer;
-                - Check-Out - customer has checked in but already departed; 
-                - No-Show - customer did not check-in and did inform the hotel of the reason why
+            Dernier statut de la réservation, supposant l'une des trois catégories suivantes :
+                - Canceled - la réservation a été annulée par le client ;
+                - Check-Out - le client s'est enregistré mais a déjà quitté l'hôtel ; 
+                - No-Show - le client ne s'est pas enregistré et a informé l'hôtel de la raison de son absence.
         """
         )
         
         st.markdown("""
         > 32. **'reservation_status_date'** : 
-            Date at which the last status was set. 
-            This variable can be used in conjunction with the ReservationStatus to understand when was the booking canceled 
-                or when did the customer checked-out of the hotel.
-                """)
+            Date à laquelle le dernier statut a été défini. 
+            Cette variable peut être utilisée conjointement avec ReservationStatus pour savoir quand la réservation a été annulée 
+            ou quand le client a quitté l'hôtel. 
+            """)
 
 
 
